0450c1abadd83a8ee6d76c9930a3cb0bc9d014bc
Abdelrhman Sayed <abdelrhman95sayed@gmail.com>
2023-08-05 04:03:53 +0300

    mainmenu first-commit

--- MainMenu.sh
+++ MainMenu.sh
@@ -1,1 +1,60 @@
-echo "hello bash"
+while [ True ]
+do
+    echo "Enter your Choice: "
+    echo "*********************"
+
+    select choice in "Create Database" "Connect Database" "Show Databases" "Drop Database" "Exit"
+    do 
+        case $choice in 
+                    "Create Database" ) 
+
+                    clear 
+                    echo "****************"
+                    read -p "Enter a Database name please: " databasename
+
+                    break ;;
+
+                    "Connect Database" )
+                    clear 
+                    echo "****************"
+                    read -p "Enter a Database to connect please: " databasename
+
+                    break ;;
+                    
+                    "Show Databases" )
+                    clear 
+                    echo "****************"
+                    echo "Here are the avilable Databases"
+
+                    read -p "Enter a Database name to show it please: " databasename
+                    break ;;
+
+                    "Drop Dataabse" )
+                    clear 
+                    echo "****************"
+                    read -p "Enter a Database to drop it please: " databasename
+
+                    break ;;
+
+                    "Exit" )
+                    clear
+                    Exit;;
+
+                    * )
+                    clear 
+                    echo "Invalid Choice, Please try again"
+                    break ;;
+        esac
+    done
+done
+
+
+
+
+
+
+
+
+
+
+

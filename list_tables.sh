9fd44a070b4166dd974d1866b0c018578ef59e35
Abdelrhman Sayed <abdelrhman95sayed@gmail.com>
2023-08-08 00:10:09 +0300

    list_table fun  added

--- list_tables.sh
+++ list_tables.sh
@@ -1,8 +1,30 @@
 #!usr/bin/bash
 
 
-ListTables() {
+List_Tables() {
+
+    #Get List of tables, excluding meta files 
+    tables=$(ls | grep -v ".meta")
 
 
-    
+    #Check if any tabels exist
+    if [ -z "$tables" ]; then 
+        echo "No tabels found"
+        return 
+    fi 
+
+    #print header 
+    echo "Tables: "
+
+    #print each table on a new line 
+    while read -r table; 
+    do  
+        echo "$table"
+
+    done <<< "$tables"
+
+
+
+
+
 }

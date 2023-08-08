b70c8c3eb4b11ada3d045a67c87017fe3c12d226
Abdelrhman Sayed <abdelrhman95sayed@gmail.com>
2023-08-09 02:45:54 +0300

    version-1

--- drop_table.sh
+++ drop_table.sh
@@ -2,5 +2,30 @@
 
 Drop_table(){
 
-    
+    #Require one arg - the table name 
+    if [ $# -ne 1 ]; then 
+        echo "Error: Please provide one table name to drop."
+        return 1
+    fi
+
+    #check if table exists 
+    if [ -f "$table_name" ]; then 
+        echo "Error: Table '$table_name' does not exist."
+        return 1
+    fi 
+
+
+
+    #confirm if user wants to drop
+    read -p "Are you sure you want to drop table "$table_name"? [y/n]
+    if [ ! confirm =~ ^[Yy]$ ]; then 
+        echo "Table drop cancelled"
+        return 0 
+    fi 
+
+    #Drop table and its metadata file 
+    rm "$table_name" "$table_name.meta"
+
+    #print success message  
+    echo "Table "$table_name" dropped successfully"
 }
